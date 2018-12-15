ba519bcac6093cb485dfd1c5512c2511d63d3e70
Mikael Stenstrand <mikael.stenstrand@netlight.com>
2018-12-15 22:22:27 +0200

    add dependencies: react-native-image-picker, react-native-vector-icons, react-native-fetch-blob, buffer

--- android/app/src/main/java/com/myoutfitapp/MainApplication.java
+++ android/app/src/main/java/com/myoutfitapp/MainApplication.java
@@ -3,6 +3,10 @@ package com.myoutfitapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.amazonaws.RNAWSCognitoPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +26,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNAWSCognitoPackage(),
+            new VectorIconsPackage(),
+            new RNFetchBlobPackage(),
+            new ImagePickerPackage()
       );
     }
 
