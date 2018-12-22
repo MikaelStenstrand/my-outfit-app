6aea7abcb199c516eb29e248d3aa4946d48295a9
Mikael Stenstrand <mikael.stenstrand@netlight.com>
2018-12-22 20:09:11 +0200

    remove simulator warnings for testing purposes

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+// test purposes
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['']);
+
 AppRegistry.registerComponent(appName, () => App);
