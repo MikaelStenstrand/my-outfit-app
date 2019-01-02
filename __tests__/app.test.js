b8362860531cedf949bc3cfc321bd3acc4b5c859
Mikael Stenstrand <mikael.stenstrand@netlight.com>
2019-01-02 15:30:31 +0200

    adding dummy test

---
+++ __tests__/app.test.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+test('dummy test for testing purposes', () => {
+  expect("test").toBe("test");
+});
---
+++ jestSetup.js
@@ -0,0 +1,22 @@
+import { NativeModules as RNNativeModules } from "react-native";
+RNNativeModules.UIManager = RNNativeModules.UIManager || {};
+RNNativeModules.UIManager.RCTView = RNNativeModules.UIManager.RCTView || {};
+RNNativeModules.RNGestureHandlerModule = RNNativeModules.RNGestureHandlerModule || {
+  State: { BEGAN: "BEGAN", FAILED: "FAILED", ACTIVE: "ACTIVE", END: "END" },
+  attachGestureHandler: jest.fn(),
+  createGestureHandler: jest.fn(),
+  dropGestureHandler: jest.fn(),
+  updateGestureHandler: jest.fn(),
+
+};
+RNNativeModules.PlatformConstants = RNNativeModules.PlatformConstants || {
+  forceTouchAvailable: false
+};
+
+
+jest.mock('react-native-fetch-blob', () => {
+  return {
+    DocumentDir: () => {},
+    polyfill: () => {},
+  }
+});
